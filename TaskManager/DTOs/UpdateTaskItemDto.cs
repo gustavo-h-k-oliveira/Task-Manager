fac6dad5835eeebc71535dd8ac5c59de432387ed
gustavo-h-k-oliveira <gustavo.oliveira204@fatec.sp.gov.br>
2025-06-04 10:20:03 -0300

    Adicionando DTOs para criação, atualização e representação de itens de tarefa; removendo a classe Task.

---
+++ TaskManager/DTOs/UpdateTaskItemDto.cs
@@ -0,0 +1,9 @@
+namespace TaskManager.DTOs
+{
+    public class UpdateTaskItemDto
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public bool IsCompleted { get; set; }
+    }
+}
